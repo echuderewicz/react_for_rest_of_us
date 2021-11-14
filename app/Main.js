1d75ef3c9bc76f857a6312d52cceb6b4692d2442
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-11-13 22:22:56 -0500

    added module.hot...

--- app/Main.js
+++ app/Main.js
@@ -4,10 +4,14 @@ import ReactDOM from "react-dom";
 function ExampleComponent() {
   return (
     <div>
-      <h1>This is our app!</h1>
-      <p>The sky is blue!!!!</p>
+      <h1>This is our app, and it's a wonderful day!</h1>
+      <p>The sky is blue and pretty!!!!</p>
     </div>
   );
 }
 
 ReactDOM.render(<ExampleComponent />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}

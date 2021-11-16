3bbfdf72f9152e612c63ed8780d330dc657cee20
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-11-15 20:49:28 -0500

    added a comment in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     port: 3000,
     contentBase: path.join(__dirname, "app"),
     hot: true,
-    historyApiFallback: { index: "index.html" },
+    historyApiFallback: { index: "index.html" }, //this is important for allowing an address to be added directly into browser
   },
   module: {
     rules: [

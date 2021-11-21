4609887982760a665e67a2e634ad66e4d7c2afde
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-11-21 18:28:24 -0500

    logged in header now showing up with proper un and pw

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState();
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -12,7 +13,11 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        <HeaderLoggedIn />
+        {loggedIn ? (
+          <HeaderLoggedIn />
+        ) : (
+          <HeaderLoggedOut setLoggedIn={setLoggedIn} />
+        )}
       </div>
     </header>
   );

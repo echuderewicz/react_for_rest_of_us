cc6103e0c4fa0dd8cbb4f5521b8a4a203bffebb5
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-10 18:26:09 -0500

    post date is now been formatted correctly

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -11,7 +11,9 @@ function ProfilePosts() {
     async function fetchPosts() {
       try {
         const response = await Axios.get(`/profile/${username}/posts`);
-        console.log(response.data);
+        //console.log(response.data);
+        setPosts(response.data);
+        setIsLoading(false);
       } catch (e) {
         console.log("There was a problem mr. c");
       }
@@ -25,30 +27,23 @@ function ProfilePosts() {
 
   return (
     <div className="list-group">
-      <a href="#" className="list-group-item list-group-item-action">
-        <img
-          className="avatar-tiny"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />{" "}
-        <strong>Example Post #1</strong>
-        <span className="text-muted small">on 2/10/2020 </span>
-      </a>
-      <a href="#" className="list-group-item list-group-item-action">
-        <img
-          className="avatar-tiny"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />{" "}
-        <strong>Example Post #2</strong>
-        <span className="text-muted small">on 2/10/2020 </span>
-      </a>
-      <a href="#" className="list-group-item list-group-item-action">
-        <img
-          className="avatar-tiny"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />{" "}
-        <strong>Example Post #3</strong>
-        <span className="text-muted small">on 2/10/2020 </span>
-      </a>
+      {posts.map((post) => {
+        const date = new Date(post.createdDate);
+        const dateFormatted = `${
+          date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()}`;
+        return (
+          <a
+            key={post._id}
+            href="#"
+            className="list-group-item list-group-item-action"
+          >
+            <img className="avatar-tiny" src="post.author.avatar" />{" "}
+            <strong>{post.title}</strong>{" "}
+            <span className="text-muted small">on {dateFormatted}</span>
+          </a>
+        );
+      })}
     </div>
   );
 }

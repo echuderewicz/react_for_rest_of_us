e216c0d33e58bc84ecb6cda6fe56ba6f152c732c
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-10 17:57:31 -0500

    can now send posts and recieving correct data back

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Axios from "axios";
 
 function ProfilePosts() {
   const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const { username } = useParams();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/posts`);
+        console.log(response.data);
+      } catch (e) {
+        console.log("There was a problem mr. c");
+      }
+    }
+    fetchPosts();
+  }, []);
 
   if (isLoading) {
     return <div>Loading Until The End of Time...</div>;
   }
+
   return (
     <div className="list-group">
       <a href="#" className="list-group-item list-group-item-action">

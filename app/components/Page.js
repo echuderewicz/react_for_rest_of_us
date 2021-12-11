0a41932c4e301bc5c1e1a8e6e759be5d4c3fc9bf
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-10 20:24:10 -0500

    single post info now displaying along with avatar and name links

--- app/components/Page.js
+++ app/components/Page.js
@@ -6,7 +6,9 @@ function Page(props) {
   useEffect(() => {
     document.title = `${props.title} | ComplexApp`;
     window.scrollTo(0, 0);
-  }, []);
+    //this addition below will instead run everytime the title changes
+    //when it was only an empty [], it only ran the first render time
+  }, [props.title]);
 
   //The container component is just a wrapper around the content
   //to display it either "wide" or normal.

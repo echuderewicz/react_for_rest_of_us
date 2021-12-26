561e1ad0e943af56e1b180e8737c8ae9896ef5e4
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-26 13:20:11 -0500

    refactor working so far

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,6 +5,7 @@ import { useImmer } from "use-immer";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import Axios from "axios";
 import { Link } from "react-router-dom";
+import Post from "./Post";
 //import DispatchContext from "../DispatchContext";
 
 function Home() {
@@ -55,24 +56,7 @@ function Home() {
           <h2>The Latest from those you follow</h2>
           <div className="list-group">
             {state.feed.map((post) => {
-              const date = new Date(post.createdDate);
-              const dateFormatted = `${
-                date.getMonth() + 1
-              }/${date.getDate()}/${date.getFullYear()}`;
-
-              return (
-                <Link
-                  key={post._id}
-                  to={`/post/${post._id}`}
-                  className="list-group-item list-group-item-action"
-                >
-                  <img className="avatar-tiny" src="post.author.avatar" />{" "}
-                  <strong>{post.title}</strong>{" "}
-                  <span className="text-muted small">
-                    by {post.author.username} on {dateFormatted}
-                  </span>
-                </Link>
-              );
+              return <Post post={post} key={post._id} />;
             })}
           </div>
         </>

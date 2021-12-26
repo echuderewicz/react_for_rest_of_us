bb9355874925c653c4880ba5d9c1dd157f069f11
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-26 12:59:31 -0500

    home page feed now working without errors

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,9 +3,13 @@ import Page from "./Page";
 import StateContext from "../StateContext";
 import { useImmer } from "use-immer";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import DispatchContext from "../DispatchContext";
 
 function Home() {
   const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
   const [state, setState] = useImmer({
     isLoading: true,
     feed: [],
@@ -46,15 +50,51 @@ function Home() {
 
   return (
     <Page title="your feed">
-      <h2 className="text-center">
-        Hello <strong>{appState.user.username}</strong>, your feed is empty.
-      </h2>
-      <p className="lead text-muted text-center">
-        Your feed displays the latest posts from the people you follow. If you
-        don&rsquo;t have any friends to follow that&rsquo;s okay; you can use
-        the &ldquo;Search&rdquo; feature in the top menu bar to find content
-        written by people with similar interests and then follow them.
-      </p>
+      {state.feed.length > 0 && (
+        <>
+          <h2>The Latest from those you follow</h2>
+          <div className="list-group">
+            {state.feed.map((post) => {
+              const date = new Date(post.createdDate);
+              const dateFormatted = `${
+                date.getMonth() + 1
+              }/${date.getDate()}/${date.getFullYear()}`;
+
+              return (
+                <Link
+                  onClick={() => {
+                    appDispatch({ type: "closeSearch" });
+                  }}
+                  key={post._id}
+                  to={`/post/${post._id}`}
+                  className="list-group-item list-group-item-action"
+                >
+                  <img className="avatar-tiny" src="post.author.avatar" />{" "}
+                  <strong>{post.title}</strong>{" "}
+                  <span className="text-muted small">
+                    by {post.author.username} on {dateFormatted}
+                  </span>
+                </Link>
+              );
+            })}
+          </div>
+        </>
+      )}
+      {state.feed.length == 0 && (
+        // jsx expression can have only (1) top level element, hence the fragment below <></>
+        <>
+          <h2 className="text-center">
+            Hello <strong>{appState.user.username}</strong>, your feed is empty.
+          </h2>
+          <p className="lead text-muted text-center">
+            Your feed displays the latest posts from the people you follow. If
+            you don&rsquo;t have any friends to follow that&rsquo;s okay; you
+            can use the &ldquo;Search&rdquo; feature in the top menu bar to find
+            content written by people with similar interests and then follow
+            them.
+          </p>
+        </>
+      )}
     </Page>
   );
 }

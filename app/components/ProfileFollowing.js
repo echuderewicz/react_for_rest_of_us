c5146a42638b83f0ba83bd12e8eafd82ccf22e5b
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-26 13:38:45 -0500

    no author is true

--- app/components/ProfileFollowing.js
+++ app/components/ProfileFollowing.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import Post from "./Post";
 
 function ProfileFollowing() {
   const [isLoading, setIsLoading] = useState(true);

d03c762adc91ad4ca8d561583a3f295bbcb1a474
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-29 23:31:03 -0500

    working

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -60,36 +60,37 @@ function Chat() {
         </span>
       </div>
       <div id="chat" className="chat-log">
-        {/* chat self */}
+        {state.chatMessages.map((message, index) => {
+          if (message.username == appState.user.username) {
+            return (
+              <div className="chat-self">
+                <div className="chat-message">
+                  <div className="chat-message-inner">{message.message}</div>
+                </div>
+                <img className="chat-avatar avatar-tiny" src={message.avatar} />
+              </div>
+            );
+          }
 
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img
-            className="chat-avatar avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />
-        </div>
-
-        {/* chat other */}
-
-        <div className="chat-other">
-          <a href="#">
-            <img
-              className="avatar-tiny"
-              src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-            />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
+          return (
+            <div className="chat-other">
               <a href="#">
-                <strong>barksalot:</strong>
+                <img
+                  className="avatar-tiny"
+                  src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
+                />
               </a>
-              Hey, I am good, how about you?
+              <div className="chat-message">
+                <div className="chat-message-inner">
+                  <a href="#">
+                    <strong>barksalot:</strong>
+                  </a>
+                  Hey, I am good, how about you?
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
       <form
         onSubmit={handleSubmit}

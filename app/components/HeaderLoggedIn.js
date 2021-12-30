1b8578109925eccf036d438969234770a31c94b9
Eric J. Chuderewicz <echuderewicz@gmail.com>
2021-12-29 20:04:28 -0500

    chat is now toggling

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -29,6 +29,9 @@ function HeaderLoggedIn(props) {
       {"   "}
 
       <span
+        onClick={() => {
+          appDispatch({ type: "toggleChat" });
+        }}
         data-for="chat"
         data-tip="Chat"
         className="mr-2 header-chat-icon text-white"
